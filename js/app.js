b1d3ba30f4049ea170088fa0a606c895e2396215
mrtrvl <mrt@mrt.ee>
2024-02-29 16:44:58 +0200

    Add activities API

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
-console.log('Hello world!');
+// load new activity on onload
+const showText = (text) => {
+  activityText.textContent = text;
+}
+
+window.onload = async () => {
+  const activity = await getActivity();
+  showText(activity.activity);
+};
+
+const activityUrl = 'https://www.boredapi.com/api/activity';
+
+const getActivity = async () => {
+  const activity = await axios.get(activityUrl);
+  return activity.data;
+}
+
+const activityButton = document.querySelector('.activity-button');
+console.log(activityButton);
+const activityText = document.querySelector('.activity-text');
+console.log(activityText);
+
+activityButton.addEventListener('click', async () => {
+  const activity = await getActivity();
+  showText(activity.activity);
+});
